51326191503fb8d220cd8a518f9f0ee205c8a7fd
Ashwani Rajput <ashwanir461@gmail.com>
2022-11-25 09:09:45 +0530

    done with the all works

--- src/main/java/com/jap/demo/BejC1S4SpringBootPc1Application.java
+++ src/main/java/com/jap/demo/BejC1S4SpringBootPc1Application.java
@@ -1,14 +1,20 @@
 package com.jap.demo;
 
+import com.jap.demo.service.MessageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BejC1S4SpringBootPc1Application {
 
+	private static MessageService messageService;
 	public static void main(String[] args) {
 
-		SpringApplication.run(BejC1S4SpringBootPc1Application.class, args);
+		ApplicationContext context = SpringApplication.run(BejC1S4SpringBootPc1Application.class, args);
+		messageService = (MessageService) context.getBean("messageService");
+		String msg = messageService.helloWorld();
+		System.out.println(msg);
 	}
 
 }
--- src/main/java/com/jap/demo/service/MessageService.java
+++ src/main/java/com/jap/demo/service/MessageService.java
@@ -1,5 +1,11 @@
 package com.jap.demo.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MessageService {
 
+    public String helloWorld() {
+        return "Good day to all";
+    }
 }
